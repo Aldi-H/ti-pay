b526b5b197895be3d913215bc0b4e7eb68adcf21
Aldi-H <aldihimawan88@gmail.com>
2022-12-03 20:36:52 +0700

    feat: add cash transfer page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Hero from "./components/Hero";
 import LandingPage from "./pages/LandingPage";
 import BankTransferLayout from "./components/BankTransferLayout";
 import CreditTransferLayout from "./components/CreditTransferLayout";
+import CashTransferLayout from "./components/CashTransferLayout";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
         <Route path="/:transactionId" element={<LandingPage />} />
         <Route path="/bankTransfer" element={<BankTransferLayout />} />
         <Route path="/creditTransfer" element={<CreditTransferLayout />} />
+        <Route path="/cashTransfer" element={<CashTransferLayout />} />
       </Routes>
     </ChakraProvider>
   );
---
+++ src/components/CashTransferLayout.jsx
@@ -0,0 +1,11 @@
+import { Heading } from "@chakra-ui/react";
+
+const CashTransferLayout = () => {
+  return (
+    <>
+      <Heading>Cash Transfer</Heading>
+    </>
+  );
+};
+
+export default CashTransferLayout;
